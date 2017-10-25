6aa6366a5fac6260aaa79f13fc2e014b144f4473
Slava <slava@panorays.com>
2017-10-25 10:49:21 +0300

    feat(engine): Place issues at the head part

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # cz-conventional-changelog
 
-Status:
-[![npm version](https://img.shields.io/npm/v/cz-conventional-changelog.svg?style=flat-square)](https://www.npmjs.org/package/cz-conventional-changelog)
-[![npm downloads](https://img.shields.io/npm/dm/cz-conventional-changelog.svg?style=flat-square)](http://npm-stat.com/charts.html?package=cz-conventional-changelog&from=2015-08-01)
-[![Build Status](https://img.shields.io/travis/commitizen/cz-conventional-changelog.svg?style=flat-square)](https://travis-ci.org/commitizen/cz-conventional-changelog)
-
 Part of the [commitizen](https://github.com/commitizen/cz-cli) family. Prompts for [conventional changelog](https://github.com/stevemao/conventional-changelog-angular/blob/master/index.js) standard.
+
+The only change from the original package is that i place the ISSUES section at the `head` part instead of the footer.
+
+When using Bitbucket it allows JIRA issues to be auto-highlighted
--- engine.js
+++ engine.js
@@ -1,53 +1,23 @@
-"format cjs";
+const wrap = require('word-wrap');
+const map = require('lodash.map');
+const padEnd = require('lodash.padend');
+const longest = require('longest');
 
-var wrap = require('word-wrap');
-var map = require('lodash.map');
-var longest = require('longest');
-var rightPad = require('right-pad');
+module.exports = function engine(options) {
+  const types = options.types;
 
-var filter = function(array) {
-  return array.filter(function(x) {
-    return x;
-  });
-};
-
-// This can be any kind of SystemJS compatible module.
-// We use Commonjs here, but ES6 or AMD would do just
-// fine.
-module.exports = function (options) {
-
-  var types = options.types;
-
-  var length = longest(Object.keys(types)).length + 1;
-  var choices = map(types, function (type, key) {
+  const length = longest(Object.keys(types)).length + 1;
+  const choices = map(types, (type, key) => {
     return {
-      name: rightPad(key + ':', length) + ' ' + type.description,
+      name: `${padEnd(`${key}:`, length)} ${type.description}`,
       value: key
     };
   });
 
   return {
-    // When a user runs `git cz`, prompter will
-    // be executed. We pass you cz, which currently
-    // is just an instance of inquirer.js. Using
-    // this you can ask questions and get answers.
-    //
-    // The commit callback should be executed when
-    // you're ready to send back a commit template
-    // to git.
-    //
-    // By default, we'll de-indent your commit
-    // template and will keep empty lines.
     prompter: function(cz, commit) {
       console.log('\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n');
 
-      // Let's ask some questions of the user
-      // so that we can populate our commit
-      // template.
-      //
-      // See inquirer.js docs for specifics.
-      // You can also opt to use another input
-      // collection library if you prefer.
       cz.prompt([
         {
           type: 'list',
@@ -92,10 +62,8 @@ module.exports = function (options) {
           }
         }
       ]).then(function(answers) {
-
-        var maxLineWidth = 100;
-
-        var wrapOptions = {
+        const maxLineWidth = 100;
+        const wrapOptions = {
           trim: true,
           newline: '\n',
           indent:'',
@@ -103,25 +71,25 @@ module.exports = function (options) {
         };
 
         // parentheses are only needed when a scope is present
-        var scope = answers.scope.trim();
-        scope = scope ? '(' + answers.scope.trim() + ')' : '';
+        let scope = answers.scope.trim();
+        scope = scope ? `(${answers.scope.trim()})` : '';
+
+        const issues = wrap(answers.issues, wrapOptions);
 
         // Hard limit this line
-        var head = (answers.type + scope + ': ' + answers.subject.trim()).slice(0, maxLineWidth);
+        const head = `${answers.type}${scope}: ${issues} ${answers.subject.trim()}`.slice(0, maxLineWidth);
 
         // Wrap these lines at 100 characters
-        var body = wrap(answers.body, wrapOptions);
+        const body = wrap(answers.body, wrapOptions);
 
         // Apply breaking change prefix, removing it if already present
         var breaking = answers.breaking ? answers.breaking.trim() : '';
         breaking = breaking ? 'BREAKING CHANGE: ' + breaking.replace(/^BREAKING CHANGE: /, '') : '';
         breaking = wrap(breaking, wrapOptions);
 
-        var issues = answers.issues ? wrap(answers.issues, wrapOptions) : '';
+        const footer = breaking || '';
 
-        var footer = filter([ breaking, issues ]).join('\n\n');
-
-        commit(head + '\n\n' + body + '\n\n' + footer);
+        commit(`${head}\n\n${body}\n\n${footer}`);
       });
     }
   };
