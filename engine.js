5bf93389d3bfe5987ad1fb66cb51c51279dce1ca
Slava <slava@panorays.com>
2017-12-27 16:21:02 +0200

    fix: Fix issues beeing set as undefined instead of empty string

--- engine.js
+++ engine.js
@@ -74,8 +74,7 @@ module.exports = function engine(options) {
         let scope = answers.scope.trim();
         scope = scope ? `(${answers.scope.trim()})` : '';
 
-        let issues = wrap(answers.issues, wrapOptions);
-        issues = issues ? `${issues} ` : issues;
+        const issues = answers.issues ? wrap(answers.issues, wrapOptions) : '';
 
         // Hard limit this line
         const head = `${answers.type}${scope}: ${issues}${answers.subject.trim()}`.slice(0, maxLineWidth);
